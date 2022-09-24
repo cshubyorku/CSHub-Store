e5fa46bd756e46e790ea9d072f4df64660985723
Shafin Mahmud <91167955+ShafinMahmudCS@users.noreply.github.com>
2022-09-24 19:37:36 -0400

    Store layout

---
+++ Script/Store.js
@@ -0,0 +1,15 @@
+//preloader
+var loader = setInterval(function () {
+  if(document.readyState !== "complete") return;
+  clearInterval(loader);
+  document.querySelector('.spinner-wrapper').style.display = "none";
+}, 250);
+
+/*Progress bar*/
+$(document).on("scroll", function(){
+  var pixels = $(document).scrollTop();
+  var pageHeight = $(document).height() - $(window).height();
+  var progress = 100 * pixels / pageHeight;
+  
+  $("div.progress").css("width", progress + "%");
+})
